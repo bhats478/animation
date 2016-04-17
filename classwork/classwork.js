896e18086b21f5962fa1ba866775d6bbcee36af2
bhats478 <bhats478@newschool.edu>
2016-04-17 16:43:44 -0400

    finished class work portion of assignment - bouncing ball

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -1,7 +1,12 @@
 /*
   Classwork
 */
+//3: Make the balls change color when you click on the drawing.
+//Hint --> You’re going to need a variable to keep track of your color,
+//and an if statement to detect if the mouse is pressed.
+
 var ballX = 0;
+var ballY = 0;
 var speed = 3;
 
 function setup(){
@@ -9,13 +14,33 @@ function setup(){
 }
 
 function draw(){
-  background("white");
+  if(mouseIsPressed){
+    background("black");
+  } else {
+    background("darkGrey");
+  }
+
+  stroke("mediumVioletred")
+  fill("mediumVioletred")
   ellipse(ballX, height/2, 50,50);
+  ellipse(width/2,ballY,50,50);
 
   if (ballX >= width){
     speed = -3;
+} else if (ballX <= 0) {
+      speed = 3;
+    }
+
+    ballX = ballX + speed;
+
+  if (ballY >= height) {
+    speed = -3;
+  } else if (ballY <= 0) {
+      speed = 3;
+
   }
 
-  ballX = ballX + speed;
+    ballY = ballY + speed;
+
 
 }
